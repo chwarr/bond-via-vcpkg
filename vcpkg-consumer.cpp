78539bf603a334220dc8fed6df5cff1250fdb86c
Christopher Warrington <chwarr@microsoft.com>
2017-07-14 14:43:01 -0700

    Add simple project that exercises bond.lib

--- vcpkg-consumer.cpp
+++ vcpkg-consumer.cpp
@@ -1,8 +1,17 @@
 #include <SDKDDKVer.h>
 #include <stdio.h>
 
+#include <bond/core/bond_types.h>
+#include <bond/core/bond_apply.h>
+#include <bond/core/runtime_schema.h>
+#include <bond/core/schema.h>
+
 int main()
 {
-    puts("hello world\n");
+    const bond::SchemaDef& schema = bond::GetRuntimeSchema<bond::SchemaDef>().GetSchema();
+    printf(
+        "SchemaDef struct qualified name: %s\n",
+        schema.structs[schema.root.struct_def].metadata.qualified_name.c_str());
+
     return 0;
 }
